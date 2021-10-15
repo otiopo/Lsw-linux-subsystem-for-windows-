ee5c2e27a8765ddc0ef51dfb206a37077693515c
otiopo <76260281+otiopo@users.noreply.github.com>
2021-10-15 10:14:14 +0000

    Ok

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import os, json
+import os, json, platform, GPUtil, psutil, threading, time
 from commands import etc
+
+def get_size(bytes, suffix="B"):
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
 
 dire = ""
 
@@ -8,6 +15,45 @@ with open("metadata.json", "w") as file:
   file.close()
 
 print("Lsw 1.0")
+
+print("Running Post Check (press enter to cancel!)")
+
+isCancelled = False
+
+def cancel():
+  input()
+  isCancelled = True
+threading.Thread(target=cancel).start()
+count = 6
+
+while isCancelled == False:
+  count -= 1
+  if count == 0:
+    break
+  if isCancelled == True:
+    break
+  os.system("clear")
+  print("Lsw 1.0")
+  print("Running Post Check (press enter to cancel!)")
+  print(str(count) + ".")
+  threading.Thread(target=cancel).start()
+  time.sleep(1)
+
+print("POST Check...")
+
+print(platform.system() + " (Build " + platform.version() + ")")
+
+gpu = GPUtil.getGPUs()
+
+if len(gpu) > 1:
+  print("Multiple GPUs detected!")
+elif len(gpu) == 0:
+  print("No GPU(s) Found!")
+
+for gp in gpu:
+  print(gp.name)
+
+print(get_size(psutil.virtual_memory().total) + " Of Ram")
 
 while True:
   if etc.hasBsoded() == "yes":
